8f9d8859a4cbcead87378af8b8222397468e2328
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-07 13:54:24 +0300

    Initial Commit

---
+++ HowToBuildALoginScreen/HowToBuildALoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToBuildALoginScreenApp.swift
+//  HowToBuildALoginScreen
+//
+//  Created by ramil on 07.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToBuildALoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
